4919cc34bb4695f9da20ed734759cfc39057da35
psychohaxer <apclead69@gmail.com>
2021-01-28 17:29:11 +0700

    @main.py fix main function

--- main.py
+++ main.py
@@ -151,7 +151,7 @@ class Laser:
 def collide(obj1, obj2):
     offset_x = obj2.x - obj1.x
     offset_y = obj2.y - obj1.y
-    
+
     return obj1.mask.overlap(obj2.mask, (offset_x, offset_y)) != None
 
 def main():
@@ -183,10 +183,10 @@ def main():
         lives_label = main_font.render(f"Lives: {lives}", 1, (255,255,255))
         level_label = main_font.render(f"Level: {level}", 1, (255,255,255))
 
-        WIN.blit(lives_label, (10,10))
+        WIN.blit(lives_label, (10, 10))
         WIN.blit(level_label, (WIDTH - level_label.get_width() - 10, 10))
 
-        for enemy in enemies: 
+        for enemy in enemies:
             enemy.draw(WIN)
 
         player.draw(WIN)
@@ -216,13 +216,13 @@ def main():
             wave_length += 5
 
             for i in range(wave_length):
-                enemy = Enemy(random.randrange(50, WIDTH-100), random.randrange(-1000, -100), random.choice(["red", "blue", "green"]))
+                enemy = Enemy(random.randrange(50, WIDTH-100), random.randrange(-1500, -100), random.choice(["red", "blue", "green"]))
                 enemies.append(enemy)
 
-        ## check for event and get it
+        ## check for event and do something
         for event in pygame.event.get():
-            if event.type == pygame.QUIT: ## to handle quit
-                run = False
+            if event.type == pygame.QUIT:
+                quit()
 
         keys = pygame.key.get_pressed()
         if keys[pygame.K_a] and player.x - player_vel > 0: ## moving left
@@ -243,7 +243,10 @@ def main():
             if random.randrange(0, 2*60) == 1:
                 enemy.shoot()
 
-            if enemy.y + enemy.get_height() > HEIGHT:
+            if collide(enemy, player):
+                player.health -= 10
+                enemies.remove(enemy)
+            elif enemy.y + enemy.get_height() > HEIGHT:
                 lives -= 1
                 enemies.remove(enemy)
 
