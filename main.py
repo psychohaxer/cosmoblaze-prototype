df734da14b13b96e131810643d63f363ce946293
psychohaxer <apclead69@gmail.com>
2021-01-28 17:22:45 +0700

    @main.py class Player add healthbar

--- main.py
+++ main.py
@@ -96,6 +96,14 @@ class Player(Ship):
                     if laser.collision(obj):
                         objs.remove(obj)
                         self.lasers.remove(laser)
+    
+    def draw(self, window):
+        super().draw(window)
+        self.healthbar(window)
+
+    def healthbar(self, window):
+        pygame.draw.rect(window, (255,0,0), (self.x, self.y + self.ship_img.get_height() + 10, self.ship_img.get_width(), 10))
+        pygame.draw.rect(window, (0,255,0), (self.x, self.y + self.ship_img.get_height() + 10, self.ship_img.get_width() * (self.health/self.max_health), 10))
             
 
 ## Blueprints for enemy ship
