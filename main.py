f3552e404ae6f05e8008beff74765c87078e2239
psychohaxer <apclead69@gmail.com>
2021-01-28 14:58:20 +0700

    @main.py add class ship and movement control

--- main.py
+++ main.py
@@ -3,34 +3,64 @@ import os
 import time
 import random
 
-WIDTH, HEIGHT = 750, 750
-WIN = pygame.display.set_mode((WIDTH,HEIGHT))
-pygame.display.set_caption("Cosmoblaze Prototype")
+pygame.font.init()
 
-## This one is image for player's ship
-YELLOW_SPACE_SHIP = pygame.image.load(os.path.join("assets","pixel_ship_yellow.png"))
+WIDTH, HEIGHT = 1280, 720
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Space Shooter Tutorial")
 
-## This is images for enemy ship
-RED_SPACE_SHIP = pygame.image.load(os.path.join("assets","pixel_ship_red_small.png"))
-GREEN_SPACE_SHIP = pygame.image.load(os.path.join("assets","pixel_ship_green_small.png"))
-BLUE_SPACE_SHIP = pygame.image.load(os.path.join("assets","pixel_ship_blue_small.png"))
+# Enemy ship
+RED_SPACE_SHIP = pygame.image.load(os.path.join("assets", "pixel_ship_red_small.png"))
+GREEN_SPACE_SHIP = pygame.image.load(os.path.join("assets", "pixel_ship_green_small.png"))
+BLUE_SPACE_SHIP = pygame.image.load(os.path.join("assets", "pixel_ship_blue_small.png"))
 
-## Lasers
-RED_LASER = pygame.image.load(os.path.join("assets","pixel_laser_red.png"))
-GREEN_LASER = pygame.image.load(os.path.join("assets","pixel_laser_green.png"))
-BLUE_LASER = pygame.image.load(os.path.join("assets","pixel_laser_blue.png"))
-YELLOW_LASER = pygame.image.load(os.path.join("assets","pixel_laser_yellow.png"))
+# Player's ship
+YELLOW_SPACE_SHIP = pygame.image.load(os.path.join("assets", "pixel_ship_yellow.png"))
 
-## Background
-BG = pygame.image.load(os.path.join("assets","background-black.png"))
+# Lasers
+RED_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_red.png"))
+GREEN_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_green.png"))
+BLUE_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_blue.png"))
+YELLOW_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_yellow.png"))
+
+# Background
+BG = pygame.transform.scale(pygame.image.load(os.path.join("assets", "background-black.png")), (WIDTH, HEIGHT))
+
+## Abstract Class for Ship
+class Ship:
+    def __init__(self, x, y, health=100):
+        self.x = x
+        self.y = y
+        self.health = health
+        self.ship_img = None
+        self.laser_img = None
+        self.lasers = []
+        self.cool_down_counter = 0
+
+    def draw(self, window):
+        pygame.draw.rect(window, (255,0,0), (self.x, self.y, 50, 50))
+        
 
 def main():
     run = True
     FPS = 60
+    level = 1
+    lives = 5
+    player_vel = 5
+
+    main_font = pygame.font.SysFont("comicsans", 50)
     clock = pygame.time.Clock()
 
     def redraw_window():
         WIN.blit(BG, (0,0))
+
+        ## Draw text on screen
+        lives_label = main_font.render(f"Lives: {lives}", 1, (255,255,255))
+        level_label = main_font.render(f"Level: {level}", 1, (255,255,255))
+
+        WIN.blit(lives_label, (10,10))
+        WIN.blit(level_label, (WIDTH - level_label.get_width() - 10, 10))
+
         pygame.display.update()
 
     while run:
@@ -39,7 +69,16 @@ def main():
 
         ## check for event and get it
         for event in pygame.event.get():
-            if event.type == pygame: ## to handle quit
+            if event.type == pygame.QUIT: ## to handle quit
                 run = False
-            #if event.type == pygame.KEYDOWN
 
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_a] and ship.x - player_vel > 0: ## moving left
+            ship.x -= player_vel
+        if keys[pygame.K_d] and ship.x + player_vel < WIDTH: ## moving right
+            ship.x += player_vel
+        if keys[pygame.K_w] and ship.y - player_vel > 0: ## moving up
+            ship.y -= player_vel
+        if keys[pygame.K_s] and ship.y + player_vel < HEIGHT: ## moving down
+            ship.y += player_vel
